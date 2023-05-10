26887431c0188bb81e96815e96ca46f2065007b6
JackyBahary <75601152+JackyBahary@users.noreply.github.com>
2023-05-11 00:27:27 +1000

    Scene 23-24 Done ("Middle" Finished)

--- Assets/shootingSensor.cs
+++ Assets/shootingSensor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class idleSensor : MonoBehaviour
+public class shootingSensor : MonoBehaviour
 {
     public Animator controller;
 
@@ -20,10 +20,7 @@ public class idleSensor : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        controller.SetBool("idleReady", true);
-        controller.SetBool("walkReady", false);
-        controller.SetBool("lookAroundReady", false);
-        controller.SetBool("inspectReady", false);
-        controller.SetBool("shootingReady", false);
+        controller.SetBool("shootingReady", true);
+        controller.SetBool("idleReady", false);
     }
 }
